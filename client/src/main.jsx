682de7f1317043893df0b69c4096d92f79f10a94
08JatinSingh <jatinsingh2824@gmail.com>
2026-08-12 13:37:50 +0530

    new

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,9 +11,12 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 
-
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  <ClerkProvider
+    publishableKey={PUBLISHABLE_KEY}
+    signInFallbackRedirectUrl="/"
+    signUpFallbackRedirectUrl="/"
+  >
     <BrowserRouter>
       <AppProvider>
         <App />
